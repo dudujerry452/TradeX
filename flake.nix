6a2957d4e8193d3450176a527278e00b771485ec
dudujerry <dudujerry452@qq.com>
2026-03-15 20:21:34 +0800

    [env] 增加nix环境配置
    111

---
+++ flake.nix
@@ -0,0 +1,64 @@
+{
+  description = "TradeX development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        pythonPackages = pkgs.python312Packages;
+        django603 = pythonPackages.buildPythonPackage rec {
+          pname = "django";
+          version = "6.0.3";
+          format = "wheel";
+
+          src = pkgs.fetchurl {
+            url = "https://files.pythonhosted.org/packages/72/b1/23f2556967c45e34d3d3cf032eb1bd3ef925ee458667fb99052a0b3ea3a6/django-6.0.3-py3-none-any.whl";
+            hash = "sha256-Lll0RBSR3bNMPxPV56n5ewe6A79wI0wKnGi3m7sjW8M=";
+          };
+
+          nativeBuildInputs = with pythonPackages; [ wheel ];
+          propagatedBuildInputs = with pythonPackages; [
+            asgiref
+            sqlparse
+          ];
+
+          doCheck = false;
+          pythonImportsCheck = [ "django" ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            python312
+            pythonPackages.pip
+            pythonPackages.virtualenv
+            django603
+            pythonPackages.pytest
+            nodejs_22
+            pnpm
+            sqlite
+            openssl
+            pkg-config
+          ];
+
+          shellHook = ''
+            export PROJECT_ROOT="$(git rev-parse --show-toplevel 2>/dev/null || pwd)"
+            export PYTHONDONTWRITEBYTECODE=1
+
+            echo "TradeX nix shell ready (project root: $PROJECT_ROOT)"
+          '';
+        };
+      }
+    );
+}
