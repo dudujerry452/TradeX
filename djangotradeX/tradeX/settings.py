e53659723049f552cf42e529d256937ae2710759
dudujerry <dudujerry452@qq.com>
2026-03-17 16:25:05 +0800

    [env] 配置同源部署和热更新.

--- djangotradeX/tradeX/settings.py
+++ djangotradeX/tradeX/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+VUE_DIST_DIR = BASE_DIR.parent / 'frontend' / 'dist'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'tradeX.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [VUE_DIST_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,3 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    VUE_DIST_DIR,
+]
